f2cca33dc8ab5b388200a882b9986e5bb63cdfa0
zigner.song <32952304+zigner-song@users.noreply.github.com>
2018-07-24 16:11:26 +0800

    Create coord_radar.R

---
+++ functions/coord_radar.R
@@ -0,0 +1,10 @@
+coord_radar <- function (theta = "x", start = 0, direction = 1) 
+{
+    theta <- match.arg(theta, c("x", "y"))
+    r <- if (theta == "x") 
+        "y"
+    else "x"
+    ggproto("CordRadar", CoordPolar, theta = theta, r = r, start = start, 
+        direction = sign(direction),
+        is_linear = function(coord) TRUE)
+}
